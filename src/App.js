46133e80382fa11fcd0de10468ada007137c9bd9
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-06-14 21:20:28 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name,
+    };
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", { user })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        console.log("Success");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Person Name:
+              <input
+                type="text"
+                name="name"
+                onChange={this.handleChange}
+              ></input>
+              <button type="submit">Add Name</button>
+            </label>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
